11da2f5d042941500319ecf6508bfdf8b7de408a
Claus Nagel <cnagel@virtualcitysystems.de>
2024-02-01 11:46:50 +0100

    turn sqlbuilder into a Java module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.citydb.sqlbuilder {
+    exports org.citydb.sqlbuilder;
+    exports org.citydb.sqlbuilder.expression;
+    exports org.citydb.sqlbuilder.schema;
+    exports org.citydb.sqlbuilder.select;
+    exports org.citydb.sqlbuilder.select.join;
+    exports org.citydb.sqlbuilder.select.operator;
+    exports org.citydb.sqlbuilder.select.operator.comparison;
+    exports org.citydb.sqlbuilder.select.operator.logical;
+    exports org.citydb.sqlbuilder.select.operator.set;
+    exports org.citydb.sqlbuilder.select.orderBy;
+    exports org.citydb.sqlbuilder.select.projection;
+    exports org.citydb.sqlbuilder.update;
+}
